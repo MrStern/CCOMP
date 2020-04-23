0460a751f64ac94f70564413461ca1f0175523f4
MrStern <56228788+MrStern@users.noreply.github.com>
2020-04-22 23:26:34 -0500

    Add files via upload

---
+++ trabajo del miercoles 22/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//mi nombre es Anthony Erick Raul Gamarra Gonzales
+	  
+  //2.27 2.28 2.29
+//NO TIENEN SENTIDO NO ENTIENDO NADA
+//2.27
+char a =65;
+cout << "A \n "<<"imprime la letra mayuscula a \n" ;
+cout <<a<< "  imprime A con ascii \n ";
+
+// escribir un programa que imprima el equivalente entero de un caracter escrito en el teclado almacene la entrada en una variable chr pruevelo con mayusculas y minusculas
+
+int b;
+cout << "dame un caracter : " ;
+cin >>a ;
+
+b=a;
+cout <<"su equivalente entero es: "<<b ;
+
+//funciona con mayusculas y minusculas
+
+
+//escriba un programa que ingrese un entero de cuatro digitos, separe en sus digiros y los imprime en orden inverso separados por 2 espacios cada uno (usar un entero) ejemplo si ponen 4315 deberia imprimir el entero 5134 
+
+int a;
+
+cout <<"ingrese un entero de 4 digitos";
+cin >> a;
+
+
+
+
+
+
+
+	return 0;
+
+
+}
